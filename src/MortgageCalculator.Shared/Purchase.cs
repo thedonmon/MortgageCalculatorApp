6052474fde9c76646c7593f7f2416738173947b9
David Maman <david.maman@hotmail.com>
2020-08-05 22:35:44 -0500

    revert bad code

--- src/MortgageCalculator.Shared/Purchase.cs
+++ src/MortgageCalculator.Shared/Purchase.cs
@@ -12,8 +12,8 @@ namespace MortgageCalculator.Shared
         [JsonProperty("down_payment_percentage")]
         public double DownPaymentPercentage { get; set; }
 
-        //public double DownPayment => PurchasePrice * (DownPaymentPercentage/100);
-        //public double LoanAmount => PurchasePrice - DownPayment;
+        public double DownPayment => PurchasePrice * (DownPaymentPercentage / 100);
+        public double LoanAmount => PurchasePrice - DownPayment;
 
     }
 }
